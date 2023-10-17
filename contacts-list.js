330cca0404b3c99eae6a0a7096ad2bdb27ed6ee4
Humberto Rojas <humbertorojas.349@gmail.com>
2023-10-17 17:40:36 -0400

    proyect-2

--- contacts-list.js
+++ contacts-list.js
@@ -1,26 +1,57 @@
-let listaContactos= [
-{nombre:"Pedro perez"}, 
-{nombre:"Juan Martinez"}, 
-{nombre:"Alberto Rojas"}
-]
+let listaContactos = [
+  {
+    id: 1,
+    nombre: "Pedro",
+    apellidos: "Perez",
+    telefono: +584241536489,
+    ubicaciones: {
+      ciudad: "Caracas",
+      direccion: "la rinconada"
+    }
+  },
+  {   id: 2,
+    nombre: "juan",
+    apellidos: "Martinez",
+    telefono: +584121235896,
+    ubicaciones: {
+      ciudad: "El tigre",
+      direccion: "calle 13 sur"
+    }},
+  { id: 3,
+    nombre: "Alberto",
+    apellidos: "Rojas",
+    telefono: +584162356987,
+    ubicaciones: {
+      ciudad: "El tigre",
+      direccion: "av. 5 de julio"
+    } },
+];
 
-function agregarContacto(nuevoContacto){
-    listaContactos.push(nuevoContacto)
-    return nuevoContacto
+function agregarContacto(nuevoContacto) {
+  listaContactos.push(nuevoContacto);
+  return nuevoContacto;
 }
 
-function borrarcontacto(nombreContacto){
-    let lista = listaContactos.filter((contacto) => contacto.nombre !== nombreContacto);
+function borrarcontacto(idContacto) {
+  let lista = listaContactos.filter(
+    (contacto) => contacto.id !== idContacto
+  );
   listaContactos = lista;
 }
-function imprimirlista(){
-    console.log(listaContactos)
+function imprimirlista() {
+  console.log(listaContactos);
 }
 
-// agregarContacto({nombre: "jose guillen"})
+ agregarContacto({id: 4,
+    nombre: "Alberto",
+    apellidos: "Rojas",
+    telefono: +584162356987,
+    ubicaciones: {
+      ciudad: "El tigre",
+      direccion: "av. 5 de julio"}})
 
-// imprimirlista()
+imprimirlista()
 
-// borrarcontacto("jose guillen")
+borrarcontacto(3)
 
-// imprimirlista()
+imprimirlista()
